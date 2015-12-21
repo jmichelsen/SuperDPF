076183439d9d403b1549620943b8dd8e9a2ba595
Jesse Michelsen <jmichelsen4@gmail.com>
2015-12-21 01:16:35 -0700

    updated structure, added logging, made more robust

--- super_dpf.py
+++ super_dpf.py
@@ -1,9 +1,10 @@
+import argparse
 import boto3
-import collections
 import json
+import logging
+from logging.config import dictConfig
 import os
 import requests
-import sys
 
 from PIL import Image
 
@@ -24,41 +25,116 @@ class DPFConfigurator(object):
         'photos': '{}/sdpf_photos'.format(PROJECT)
     }
 
-    STORAGE_TYPES = ['aws', 'gphotos']
-
     def __init__(self):
-        try:
-            os.path.isfile(self.PATHS['config'])
-        except:
+        if not os.path.isfile(self.PATHS['config']):
+            logging.info('Initializing config...')
             with open(self.PATHS['conf_template'], 'r') as template:
                 template = json.load(template)
-                template['first_run'] = ''
                 with open(self.PATHS['config'], 'w') as config_file:
                     json.dump(template, config_file)
 
-            self.create_photo_dirs()
+            self._create_photo_dirs()
 
-    def create_photo_dirs(self):
+    def _create_photo_dirs(self):
         if not os.path.isdir(self.PATHS['photos']):
             os.makedirs(self.PATHS['photos'])
 
+    def _walk_json_tree(self, node, kwargs):
+        for key, val in node.items():
+            if isinstance(val, dict):
+                self._walk_json_tree(val, kwargs)
+            elif key in kwargs:
+                node[key] = kwargs.get(key)
+            else:
+                continue
+        return node
 
-class BaseDPF(DPFConfigurator):
+
+class LogManager(object):
+    LOG_LOCATION = os.path.dirname(os.path.realpath(__file__))
+    LOGGING = {
+        'version': 1,
+        'formatters': {
+            'standard': {
+                'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+                'datefmt': "%d/%b/%Y %H:%M:%S"
+            },
+            'verbose': {
+                'format': 'dpf[%(process)d]: %(levelname)s %(name)s[%(module)s] %(message)s'
+            },
+        },
+        'handlers': {
+            'null': {
+                'level': 'DEBUG',
+                'class': 'logging.NullHandler',
+            },
+            # 'mail_admins': {
+            #     'level': 'ERROR',
+            #     'class': 'logging.handlers.AdminEmailHandler'
+            # },
+            'logfile': {
+                'level': 'INFO',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'filename': '{}/sdpf.log'.format(LOG_LOCATION),
+                'maxBytes': 50000000,
+                'backupCount': 2,
+                'formatter': 'standard',
+            },
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'standard'
+            },
+            'syslog': {
+                'level': 'INFO',
+                'class': 'logging.handlers.SysLogHandler',
+                'formatter': 'verbose',
+                'address': '/dev/log',
+                'facility': 'local2',
+            }
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['console', 'syslog'],
+                'propagate': True,
+                'level': 'WARN',
+            },
+            'urllib3': {
+                'handlers': ['console', 'logfile', 'syslog'],
+                'propagate': True,
+                'level': 'ERROR',
+            },
+            '': {
+                'handlers': ['console', 'logfile', 'syslog'],
+                'propagate': True,
+                'level': 'DEBUG',
+            },
+        }
+    }
+
+    dictConfig(LOGGING)
+
+
+class BaseDPF(DPFConfigurator, LogManager):
     @property
     def settings(self):
         try:
-            os.path.isfile(self.PATHS['config'])
             with open(self.PATHS['config'], 'r') as config_file:
                 config = json.load(config_file)
             return config
         except IOError as e:
-            return e
+            logging.error(e)
+
+    def _save_settings(self, **kwargs):
+        config = self._walk_json_tree(self.settings, kwargs)
+        with open(self.PATHS['config'], 'w') as config_file:
+            json.dump(config, config_file)
 
 
 class AWSController(BaseDPF):
     @property
     def aws_dir(self):
-        return '{}/{}/'.format(self.PATHS['photos'], 'aws')
+        return '{}/{}'.format(self.PATHS['photos'], 'aws')
     
     @property
     def aws_configured(self):
@@ -66,7 +142,11 @@ class AWSController(BaseDPF):
 
     @property
     def aws_enabled(self):
-        return bool(self.settings['aws']['a_enabled'])
+        try:
+            if os.path.isfile(self.PATHS['config']):
+                return bool(self.settings['aws']['a_enabled'])
+        except RuntimeWarning as e:
+            logging.error('Settings have not been initiated. {}'.format(e))
 
     def aws_configure_dialogue(self):
         access_key = raw_input('AWS Access Key: ')
@@ -79,29 +159,40 @@ class AWSController(BaseDPF):
         if not os.path.isdir(self.aws_dir):
             os.makedirs(self.aws_dir)
 
-        self.save_settings(a_enabled=enable, access_key=access_key,
-                           secret_key=secret_key, bucket=bucket)
+        os.environ['AWS_ACCESS_KEY_ID'] = access_key
+        os.environ['AWS_SECRET_ACCESS_KEY'] = secret_key
 
-    def aws_sync(self):
+        self._save_settings(a_enabled=enable, access_key=access_key,
+                            secret_key=secret_key, bucket=bucket)
+
+    def _aws_sync(self):
         if not self.aws_configured:
-            print 'AWS is not configured.'
+            logging.info('AWS is not configured.')
             configure = raw_input('Configure it now? ')
             if configure.lower() == 'yes':
                 self.aws_configure_dialogue()
         elif not self.aws_enabled:
-            print 'AWS is not enabled.'
+            logging.info('AWS is not enabled.')
             enable = raw_input('Enable it now? ')
             enable = 'true' if enable.lower() == 'yes' else ''
-            self.save_settings(a_enabled=enable)
+            self._save_settings(a_enabled=enable)
         else:
-            bucket_name = self.settings['aws']['bucket']
+            buckets = []
             s3 = boto3.resource('s3')
-            bucket = s3.Bucket(bucket_name)
+            for s3bucket in s3.buckets.all():
+                if 'dpf' in s3bucket.name:
+                    buckets.append(s3bucket.name)
 
-            for s3object in bucket.objects.iterator():
-                filename = '{}/{}'.format(self.aws_dir, s3object.key)
-                if not os.path.isfile(filename):
-                    bucket.download_file(s3object.key, filename)
+            if self.settings['aws']['bucket'] not in buckets:
+                buckets.append(self.settings['aws']['bucket'])
+
+            for bucket in buckets:
+                s3bucket = s3.Bucket(bucket)
+                for s3object in s3bucket.objects.iterator():
+                    filename = '{}/{}'.format(self.aws_dir, s3object.key)
+                    if not os.path.isfile(filename):
+                        logging.info('Downloading {}'.format(s3object.key))
+                        bucket.download_file(s3object.key, filename)
 
 
 class GPhotoController(BaseDPF):
@@ -115,9 +206,13 @@ class GPhotoController(BaseDPF):
 
     @property
     def gphotos_enabled(self):
-        return bool(self.settings['gphotos']['g_enabled'])
+        try:
+            if os.path.isfile(self.PATHS['config']):
+                return bool(self.settings['gphotos']['g_enabled'])
+        except RuntimeWarning as e:
+            logging.error('Settings have not been initiated. {}'.format(e))
 
-    def parse_feed_url(self, url):
+    def _parse_feed_url(self, url):
         if urlsplit(url).path.startswith('/data/feed/'):
             return url
         elif urlsplit(url).netlock == 'picasaweb.google.com':
@@ -126,13 +221,13 @@ class GPhotoController(BaseDPF):
                 response.content, selfClosingTags=['meta', 'link', 'base'])
 
             for link in parsed.findAll('link'):
-                if rel in link:
+                if 'rel' in link:
                     if link['rel'] == 'alternate':
                         return link['href']
 
     def gphotos_configure_dialogue(self):
         # https://picasaweb.google.com/data/feed/base/user/107119165375640000020/albumid/6225063173331304033?alt=rss&kind=photo&authkey=Gv1sRgCPzC_v7n98aRmgE&hl=en_US
-        feed_url = self.parse_feed_url(raw_input('Google Photos Feed URL: '))
+        feed_url = self._parse_feed_url(raw_input('Google Photos Feed URL: '))
         user_id = raw_input('Google Photos Username: ')
         album_id = raw_input('Album ID: ')
         enable = raw_input('Enter "yes" to enable Google Photo sync: ')
@@ -142,45 +237,53 @@ class GPhotoController(BaseDPF):
         if not os.path.isdir(self.gphotos_dir):
             os.makedirs(self.gphotos_dir)
 
-        self.save_settings(g_enabled=enable, feed_url=feed_url,
-                           user_id=user_id, album_id=album_id)
+        self._save_settings(g_enabled=enable, feed_url=feed_url,
+                            user_id=user_id, album_id=album_id)
 
-    def gphotos_sync(self):
+    def _gphotos_sync(self):
         if not self.gphotos_configured:
-            print 'Google Photos is not configured.'
+            logging.info('Google Photos is not configured.')
             configure = raw_input('Configure it now? ')
             if configure.lower() == 'yes':
                 self.gphotos_configure_dialogue()
         elif not self.gphotos_enabled:
-            print 'Google Photos is not enabled.'
+            logging.info('Google Photos is not enabled.')
             enable = raw_input('Enable it now? ')
             enable = 'true' if enable.lower() == 'yes' else ''
-            self.save_settings(g_enabled=enable)
+            self._save_settings(g_enabled=enable)
         else:
             feed_url = self.settings['gphotos'].get('feed_url')
-            response = requests.get(feed_url)
-            tags = BeautifulStoneSoup(
-                response.content).findAll('media:content')
-            for tag in tags:
-                if tag.get('url', False):
-                    filename = os.path.join(
-                        self.gphotos_dir,
-                        os.path.basename(urlsplit(tag['url'])[2]))
-                    if not os.path.isfile(filename):
-                        print 'Getting img at {}'.format(tag['url'])
-                        response = requests.get(tag['url'], stream=True)
-                        try:
-                            img = Image.open(StringIO(response.content))
-                            img.save('{}'.format(filename))
-                        except IOError:
-                            pass
+            for feed in feed_url:
+                response = requests.get(feed)
+                tags = BeautifulStoneSoup(
+                    response.content).findAll('media:content')
+                for tag in tags:
+                    if tag.get('url', False):
+                        filename = os.path.join(
+                            self.gphotos_dir,
+                            os.path.basename(urlsplit(tag['url'])[2]))
+                        if not os.path.isfile(filename):
+                            logging.info(
+                                'Getting img at {}'.format(tag['url']))
+                            response = requests.get(tag['url'], stream=True)
+                            try:
+                                img = Image.open(StringIO(response.content))
+                                img.save('{}'.format(filename))
+                            except IOError as e:
+                                logging.error(e)
+
+
+class DropboxController(BaseDPF):
+    pass
 
 
 class FlickrController(BaseDPF):
+    # https://stuvel.eu/media/flickrapi-docs/documentation/
     pass
 
 
 class InstagramController(BaseDPF):
+    # https://github.com/Instagram/python-instagram
     pass
 
 
@@ -189,33 +292,17 @@ class HotMediaController(BaseDPF):
 
 
 class SuperDPF(AWSController, GPhotoController):
-    def walk_json_tree(self, node, kwargs):
-        for key, val in node.items():
-            if isinstance(val, dict):
-                self.walk_json_tree(val, kwargs)
-            elif key in kwargs:
-                node[key] = kwargs.get(key)
-            else:
-                continue
-        return node
-
-    def save_settings(self, **kwargs):
-        config = self.walk_json_tree(self.settings, kwargs)
-
-        with open(self.PATHS['config'], 'w') as config_file:
-            json.dump(config, config_file)
-
-    def sync(self):
+    def _sync(self):
         if self.aws_enabled:
-            print 'Syncing AWS....'
-            self.aws_sync()
+            logging.info('Syncing AWS....')
+            self._aws_sync()
 
         if self.gphotos_enabled:
-            print 'Syncing Google Photos....'
-            self.gphotos_sync()
+            logging.info('Syncing Google Photos....')
+            self._gphotos_sync()
 
     def run(self):
-        self.sync()
+        self._sync()
 
 if __name__ == "__main__":
     dpf = SuperDPF()
