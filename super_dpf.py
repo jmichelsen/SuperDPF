b3e844681f4bbd875bd9892e1207f75a63831152
Shaun Kruger <shaun.kruger@gmail.com>
2015-12-24 00:17:04 -0700

    Refactor based on notes in PR 3

--- super_dpf.py
+++ super_dpf.py
@@ -1,6 +1,7 @@
 import argparse
 import boto3
-import json
+import yaml
+import traceback
 import logging
 from logging.config import dictConfig
 import os
@@ -16,156 +17,142 @@ from BeautifulSoup import BeautifulStoneSoup
 
 requests.packages.urllib3.disable_warnings()
 
-
-class DPFConfigurator(object):
-    PROJECT = os.path.expanduser('~/SuperDPF')
-    PATHS = {
-        'conf_template': '{}/.conf_template.json'.format(PROJECT),
-        'config': '{}/config.json'.format(PROJECT),
-        'photos': '{}/sdpf_photos'.format(PROJECT)
-    }
-
-    def __init__(self):
-        if not os.path.isfile(self.PATHS['config']):
-            logging.info('Initializing config...')
-            with open(self.PATHS['conf_template'], 'r') as template:
-                template = json.load(template)
-                with open(self.PATHS['config'], 'w') as config_file:
-                    json.dump(template, config_file)
-
-            self._create_photo_dirs()
-
-    def _create_photo_dirs(self):
-        if not os.path.isdir(self.PATHS['photos']):
-            os.makedirs(self.PATHS['photos'])
-
-    def _walk_json_tree(self, node, kwargs):
-        for key, val in node.items():
-            if isinstance(val, dict):
-                self._walk_json_tree(val, kwargs)
-            elif key in kwargs:
-                node[key] = kwargs.get(key)
-            else:
-                continue
-        return node
-
-
-class LogManager(object):
-    LOG_LOCATION = os.path.dirname(os.path.realpath(__file__))
-    LOGGING = {
-        'version': 1,
-        'formatters': {
-            'standard': {
-                'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
-                'datefmt': "%d/%b/%Y %H:%M:%S"
-            },
-            'verbose': {
-                'format': 'dpf[%(process)d]: %(levelname)s %(name)s[%(module)s] %(message)s'
-            },
+LOG_LOCATION = os.path.dirname(os.path.realpath(__file__))
+LOGGING = {
+    'version': 1,
+    'formatters': {
+        'standard': {
+            'format': "[%(asctime)s] %(levelname)s [%(name)s:%(lineno)s] %(message)s",
+            'datefmt': "%d/%b/%Y %H:%M:%S"
         },
-        'handlers': {
-            'null': {
-                'level': 'DEBUG',
-                'class': 'logging.NullHandler',
-            },
-            # 'mail_admins': {
-            #     'level': 'ERROR',
-            #     'class': 'logging.handlers.AdminEmailHandler'
-            # },
-            'logfile': {
-                'level': 'INFO',
-                'class': 'logging.handlers.RotatingFileHandler',
-                'filename': '{}/sdpf.log'.format(LOG_LOCATION),
-                'maxBytes': 50000000,
-                'backupCount': 2,
-                'formatter': 'standard',
-            },
-            'console': {
-                'level': 'DEBUG',
-                'class': 'logging.StreamHandler',
-                'formatter': 'standard'
-            },
-            'syslog': {
-                'level': 'INFO',
-                'class': 'logging.handlers.SysLogHandler',
-                'formatter': 'verbose',
-                'address': '/dev/log',
-                'facility': 'local2',
-            }
+        'verbose': {
+            'format': 'dpf[%(process)d]: %(levelname)s %(name)s[%(module)s] %(message)s'
         },
-        'loggers': {
-            'django': {
-                'handlers': ['console', 'syslog'],
-                'propagate': True,
-                'level': 'WARN',
-            },
-            'urllib3': {
-                'handlers': ['console', 'logfile', 'syslog'],
-                'propagate': True,
-                'level': 'ERROR',
-            },
-            '': {
-                'handlers': ['console', 'logfile', 'syslog'],
-                'propagate': True,
-                'level': 'DEBUG',
-            },
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        # 'mail_admins': {
+        #     'level': 'ERROR',
+        #     'class': 'logging.handlers.AdminEmailHandler'
+        # },
+        'logfile': {
+            'level': 'INFO',
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': '{}/sdpf.log'.format(LOG_LOCATION),
+            'maxBytes': 50000000,
+            'backupCount': 2,
+            'formatter': 'standard',
+        },
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'standard'
+        },
+        'syslog': {
+            'level': 'INFO',
+            'class': 'logging.handlers.SysLogHandler',
+            'formatter': 'verbose',
+            'address': '/dev/log',
+            'facility': 'local2',
         }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console', 'syslog'],
+            'propagate': True,
+            'level': 'WARN',
+        },
+        'urllib3': {
+            'handlers': ['console', 'logfile', 'syslog'],
+            'propagate': True,
+            'level': 'ERROR',
+        },
+        '': {
+            'handlers': ['console', 'logfile', 'syslog'],
+            'propagate': True,
+            'level': 'DEBUG',
+        },
     }
+}
 
-    dictConfig(LOGGING)
-
-
-class BaseDPF(DPFConfigurator, LogManager):
-    @property
-    def settings(self):
-        try:
-            with open(self.PATHS['config'], 'r') as config_file:
-                config = json.load(config_file)
-            return config
-        except IOError as e:
-            logging.error(e)
-
-    def _save_settings(self, **kwargs):
-        config = self._walk_json_tree(self.settings, kwargs)
-        with open(self.PATHS['config'], 'w') as config_file:
-            json.dump(config, config_file)
+dictConfig(LOGGING)
 
 
-class AWSController(BaseDPF):
-    @property
-    def aws_dir(self):
-        return '{}/{}'.format(self.PATHS['photos'], 'aws')
-    
-    @property
-    def aws_configured(self):
-        return bool(self.settings['aws'].get('access_key', False))
+class SettingsItem:
+    def __init__(self, name, title, value_type=str,
+                 help_text=None, primary=False):
+        self.name = name
+        self.title = title
+        self._help = help_text
+        self.value_type = value_type
+        self.primary = primary
 
     @property
-    def aws_enabled(self):
-        try:
-            if os.path.isfile(self.PATHS['config']):
-                return bool(self.settings['aws']['a_enabled'])
-        except RuntimeWarning as e:
-            logging.error('Settings have not been initiated. {}'.format(e))
+    def help(self):
+        if self._help:
+            return self._help
+        else:
+            return self.title
 
-    def aws_configure_dialogue(self):
-        access_key = raw_input('AWS Access Key: ')
-        secret_key = raw_input('AWS Secret: ')
-        bucket = raw_input('Bucket containing *only* dpf pics: ')
-        enable = raw_input('Enter "yes" to enable AWS sync: ')
+    def value_dialog(self):
+        value = raw_input("{}: ".format(self.title))
+        if issubclass(self.value_type, basestring):
+            return value
+        elif self.value_type == int:
+            return int(value)
+        else:
+            print("Unsupported value type: {}".format(self.value_type))
+            return None
 
-        enable = 'true' if enable.lower() == 'yes' else ''
 
-        if not os.path.isdir(self.aws_dir):
-            os.makedirs(self.aws_dir)
+class BaseDPF(object):
+    REQUIRED_SETTINGS = list()
 
-        os.environ['AWS_ACCESS_KEY_ID'] = access_key
-        os.environ['AWS_SECRET_ACCESS_KEY'] = secret_key
+    def __init__(self, settings_object):
+        self.settings = settings_object
 
-        self._save_settings(a_enabled=enable, access_key=access_key,
-                            secret_key=secret_key, bucket=bucket)
+    @property
+    def subdir(self):
+        pk_list = [str(s) for s in self.__class__.settings_pk(self.settings)]
+        return "{}__{}".format(self.__class__.__name__,
+                               '_'.join(pk_list))
 
-    def _aws_sync(self):
+    @classmethod
+    def settings_dialog(cls):
+        settings_object = dict()
+        for item in cls.REQUIRED_SETTINGS:
+            settings_object[item.name] = item.value_dialog()
+        return settings_object
+
+    @classmethod
+    def settings_pk(cls, settings_object):
+        pk_values = sorted({i.name for i
+                            in cls.REQUIRED_SETTINGS if i.primary})
+        pk = list()
+        pk.append(cls.__name__)
+        for value in pk_values:
+            pk.append(settings_object.get(value))
+        return pk
+
+
+class AmazonS3DPF(BaseDPF):
+    REQUIRED_SETTINGS = [
+        SettingsItem('aws_access_key', 'AWS Access Key', primary=True),
+        SettingsItem('aws_secret', 'AWS Secret'),
+        SettingsItem('bucket', 'Bucket containing *only* dpf pics',
+                     primary=True),
+    ]
+
+    # def aws_configure_dialogue(self):
+        # os.environ['AWS_ACCESS_KEY_ID'] = access_key
+        # os.environ['AWS_SECRET_ACCESS_KEY'] = secret_key
+
+    def sync(self):
+        os.environ['AWS_ACCESS_KEY_ID'] = self.settings.get('aws_access_key')
+        os.environ['AWS_SECRET_ACCESS_KEY'] = self.settings.get('aws_secret')
         if not self.aws_configured:
             logging.info('AWS is not configured.')
             configure = raw_input('Configure it now? ')
@@ -199,22 +186,12 @@ class AWSController(BaseDPF):
                         bucket.download_file(s3object.key, filename)
 
 
-class GPhotoController(BaseDPF):
-    @property
-    def gphotos_dir(self):
-        return '{}/{}/'.format(self.PATHS['photos'], 'gphotos')
-
-    @property
-    def gphotos_configured(self):
-        return bool(self.settings['gphotos'].get('feed_url', False))
-
-    @property
-    def gphotos_enabled(self):
-        try:
-            if os.path.isfile(self.PATHS['config']):
-                return bool(self.settings['gphotos']['g_enabled'])
-        except RuntimeWarning as e:
-            logging.error('Settings have not been initiated. {}'.format(e))
+class GPhotoDPF(BaseDPF):
+    REQUIRED_SETTINGS = [
+        SettingsItem('feed_url', 'Feed URL'),
+        SettingsItem('user_id', 'Google Photos Username', primary=True),
+        SettingsItem('album_id', 'Album ID', primary=True),
+    ]
 
     def _parse_feed_url(self, url):
         if urlsplit(url).path.startswith('/data/feed/'):
@@ -229,22 +206,7 @@ class GPhotoController(BaseDPF):
                     if link['rel'] == 'alternate':
                         return link['href']
 
-    def gphotos_configure_dialogue(self):
-        # https://picasaweb.google.com/data/feed/base/user/107119165375640000020/albumid/6225063173331304033?alt=rss&kind=photo&authkey=Gv1sRgCPzC_v7n98aRmgE&hl=en_US
-        feed_url = self._parse_feed_url(raw_input('Google Photos Feed URL: '))
-        user_id = raw_input('Google Photos Username: ')
-        album_id = raw_input('Album ID: ')
-        enable = raw_input('Enter "yes" to enable Google Photo sync: ')
-
-        enable = 'true' if enable.lower() == 'yes' else ''
-
-        if not os.path.isdir(self.gphotos_dir):
-            os.makedirs(self.gphotos_dir)
-
-        self._save_settings(g_enabled=enable, feed_url=list(feed_url),
-                            user_id=user_id, album_id=album_id)
-
-    def _gphotos_sync(self):
+    def sync(self):
         if not self.gphotos_configured:
             logging.info('Google Photos is not configured.')
             configure = raw_input('Configure it now? ')
@@ -297,19 +259,107 @@ class HotMediaController(BaseDPF):
     pass
 
 
-class SuperDPF(AWSController, GPhotoController):
-    def _sync(self):
-        if self.aws_enabled:
-            logging.info('Syncing AWS....')
-            self._aws_sync()
+class DPFConfigurator(object):
+    PROJECT = os.path.expanduser('~/SuperDPF')
+    PATHS = {
+        'conf_template': '{}/.conf_template.json'.format(PROJECT),
+        'config': '{}/config.yml'.format(PROJECT),
+        'photos': '{}/sdpf_photos'.format(PROJECT)
+    }
 
-        if self.gphotos_enabled:
-            logging.info('Syncing Google Photos....')
-            self._gphotos_sync()
+    ACCOUNT_TYPES = [
+        AmazonS3DPF,
+        GPhotoDPF
+    ]
+
+    def __init__(self):
+        if not os.path.isfile(self.PATHS['config']):
+            logging.info('Initializing empty config...')
+            with open(self.PATHS['config'], 'w') as config_file:
+                yaml.dump(dict(), config_file)
+            self.config_dict = dict()
+
+        else:
+            with open(self.PATHS['config']) as config:
+                self.config_dict = yaml.load(config)
+
+        self.config_dict.setdefault('accounts', list())
+        self._create_photo_dirs()
+
+    def save(self):
+        # serialize before we open the file (and truncate) in write mode
+        yaml_str = yaml.dump(self.config_dict)
+        with open(self.PATHS['config'], 'w') as config:
+            config.write(yaml_str)
+
+    @property
+    def accounts(self):
+        return self.config_dict.get('accounts')
+
+    def add_account(self, klass, settings_dict):
+        if klass not in self.ACCOUNT_TYPES:
+            print("Unsupported account type! {}".format(klass.__name__))
+            return
+        pk = klass.settings_pk(settings_dict)
+        new_account_list = list()
+        replaced = False
+        for account_class, account_settings in self.accounts:
+            if pk == account_class.settings_pk(account_settings):
+                # Replace already existing
+                new_account_list.append(klass, settings_dict)
+                replaced = True
+            else:
+                new_account_list.append( (account_class, account_settings) )
+
+        if not replaced:
+            new_account_list.append( (klass, settings_dict) )
+
+        self.config_dict['accounts'] = new_account_list
+
+    def add_account_dialog(self):
+        account_types = {k.__name__: k for k in self.ACCOUNT_TYPES}
+        print("Valid account types: {}".format(', '.join(account_types)))
+        account_type = raw_input('Account type: ')
+        if account_type in account_types.keys():
+            klass = account_types[account_type]
+            settings = klass.settings_dialog()
+            self.add_account(klass, settings)
+            self.save()
+        else:
+            print("{} was not a valid account type.".format(account_type))
+
+    def _create_photo_dirs(self):
+        if not os.path.isdir(self.PATHS['photos']):
+            os.makedirs(self.PATHS['photos'])
+        for klass, settings_object in self.accounts:
+            dpf_instance = klass(settings_object)
+            account_path = os.path.join(self.PATHS['photos'],
+                                        dpf_instance.subdir())
+            os.makedirs(account_path)
+
+
+class SuperDPF(AmazonS3DPF, GPhotoDPF):
+    def __init__(self):
+        self.config = DPFConfigurator()
+
+    def _sync(self):
+        print("Syncing {} accounts".format(len(self.config.accounts)))
+        for klass, config in self.config.accounts:
+            pk = None
+            try:
+                pk = klass.settings_pk(config)
+                instance = klass(config)
+                instance.sync()
+            except Exception as e:
+                msg = "Error syncing {} ({}): {}"
+                traceback.print_exc()
+                print(msg.format(klass.__name__, pk, e))
 
     def run(self):
+        if not len(self.config.accounts):
+            self.config.add_account_dialog()
         self._sync()
-        os.system('/usr/bin/supervisorctl restart sdpf')
+        # os.system('/usr/bin/supervisorctl restart sdpf')
 
 if __name__ == "__main__":
     dpf = SuperDPF()
